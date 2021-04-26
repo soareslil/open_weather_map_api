4b4d1971d1ab61815f8bbbc03d57a1def63c4c2f
Liliane Soares <lilianesoares.code@gmail.com>
2021-04-26 12:36:34 -0300

    changes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var main = document.querySelector('#name')
 var temp = document.querySelector('.temp')
 var desc = document.querySelector('.desc')
 var clouds = document.querySelector('.clouds')
+var windspeed = document.querySelector('.winds')
 var button = document.querySelector('.submit')
 
 button.addEventListener("click", function(name){
@@ -12,12 +13,17 @@ button.addEventListener("click", function(name){
         var nameValue = data['name'];
         var descValue = data['weather'][0]['description']
         var tempValue = data['main']['temp']
+        var cloudValue = data['clouds']['all']
+        var windsValue = data['wind']['speed']
 
-        main.innerHTML = nameValue;
-        desc.innerHTML = 'Description - '+descValue;
-        temp.innerHTML = 'Temperature - '+tempValue
+
         input.value="";
-
+        main.innerHTML = nameValue;
+        temp.innerHTML = 'Temperature - '+tempValue;
+        desc.innerHTML = 'Description - '+descValue;
+        clouds.innerHTML = 'Clouds - '+cloudValue;
+        windspeed.innerHTML = 'Wind - '+windsValue;
+console.log(data)
     })
     .catch(err => alert("Wrong city name!"));
 })
